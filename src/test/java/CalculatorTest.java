73a86f7959119418fe8e688357c06500bbf4b5fd
BorisR69 <rozov69@gmail.com>
2022-12-09 23:36:16 +0300

    New commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.*;
+
+class CalculatorTest {
+
+    @Test
+    public void plus() {
+        Calculator calc = new Calculator();
+        int x = 4;
+        int y = 2;
+        int result = calc.plus.apply(x, y);
+
+        Assertions.assertEquals(6, result);
+        Assertions.assertNotEquals(4, result);
+    }
+
+    @Test
+    public void minus() {
+        Calculator calc = new Calculator();
+        int x = 4;
+        int y = 2;
+        int result = calc.minus.apply(x, y);
+
+        Assertions.assertEquals(2, result);
+        Assertions.assertNotEquals(4, result);
+    }
+
+    @Test
+    public void devide() {
+        Calculator calc = new Calculator();
+        int x = 4;
+        int y = 2;
+        int result = calc.devide.apply(x, y);
+
+        Assertions.assertEquals(2, result);
+        Assertions.assertNotEquals(4, result);
+    }
+
+    @Test
+    public void devideZero() {
+        Calculator calc = new Calculator();
+        int x = 4;
+        int y = 0;
+        int result = calc.devide.apply(x, y);
+
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    public void multiply() {
+        Calculator calc = new Calculator();
+        int x = 4;
+        int y = 8;
+        int result = calc.multiply.apply(x, y);
+
+        Assertions.assertEquals(32, result);
+    }
+}
